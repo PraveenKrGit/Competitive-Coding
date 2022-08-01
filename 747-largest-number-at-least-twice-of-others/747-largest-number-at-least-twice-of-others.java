94f3a82b51e3c4cd6fd96277d7dba66eb03377f5
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-08-01 13:49:59 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (98.27%) - LeetHub

---
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        
+        int max = -1, second = -1, index = -1;
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                second = max;
+                max = nums[i];
+                index = i;
+            }
+            
+            else if(nums[i]>second){
+                second = nums[i];
+            }
+        }
+        
+        return 2*second <= max ? index : -1;
+    }
+}
