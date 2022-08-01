0eb8449a89cd41c004303e7cf4b2af81d796d9ff
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-08-01 22:18:05 +0530

    Time: 2 ms (64.02%), Space: 41.6 MB (33.54%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.java
+++ 1006-clumsy-factorial/1006-clumsy-factorial.java
@@ -4,23 +4,26 @@ class Solution {
             return 1;
         
         if(n==2)
-            return 2;
+            return 2*1;
         
         if(n==3)
-            return 6;
+            return 3*2/1;
         
-        if(n==4)
-            return 7;
+        return n *(n-1)/(n-2)+Helper(n-3);
+    }
+    
+    private int Helper(int M){
+        if(M==1)
+            return 1;
         
-        if(n%4==1)
-            return n+2;
+        if(M==2)
+            return 2-1;
+        if(M==3)
+            return 3-2*1;
         
-        if(n%4==2)
-            return n+2;
+        if(M==4)
+            return 4-3*2/1;
         
-        if(n%4==3)
-            return n-1;
-        
-        return n+1;
+        return M-(M-1)*(M-2)/(M-3)+Helper(M-4);
     }
 }
