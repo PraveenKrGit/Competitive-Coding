66d1223829b5f16c057f575a2d82732fbf9fb6bc
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-08-05 16:21:47 +0530

    Time: 15 ms (6.60%), Space: 42.3 MB (44.10%) - LeetHub

---
+++ 537-complex-number-multiplication/537-complex-number-multiplication.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String complexNumberMultiply(String num1, String num2) {
+        String x[] = num1.split("\\+|i");
+        String y[] = num2.split("\\+|i");
+        
+        int num1_real = Integer.parseInt(x[0]);
+        int num1_img = Integer.parseInt(x[1]);
+        
+        int num2_real = Integer.parseInt(y[0]);
+        int num2_img = Integer.parseInt(y[1]);
+        
+        return(num1_real * num2_real - num1_img*num2_img) + "+" +(num1_real*num2_img + num1_img*num2_real)+"i";
+    }
+}
