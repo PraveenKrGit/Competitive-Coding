368f1f9b261dc7ce9c2b27fe35c2c8a988ebacea
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-08-25 19:44:47 +0530

    Time: 1 ms (89.64%), Space: 43.8 MB (18.42%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -2,15 +2,22 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         
+        //add [] to res
         res.add(new ArrayList<>());
         
         for(int n:nums){
             int size = res.size();
             for(int i=0;i<size;i++){
+                //get subset from res at index i
                 List<Integer> subset = new ArrayList<>(res.get(i));
                 
+                //add number to that subset
                 subset.add(n);
+                
+                //add subset to res
                 res.add(subset);
+                
+                //move to next subset
             }
         }
         
