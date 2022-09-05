1caed54b5d69e3557c826fd3ece443ac88bce0a7
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-09-05 14:34:06 +0530

    Time: 1 ms (99.97%), Space: 46.1 MB (35.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,30 +1,25 @@
 class Solution {
+    
+    static int[][] mem;
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n= grid[0].length;
+        int m = grid.length-1;
+        int n = grid[0].length-1;
         
-        int dp[][] = new int[m][n];
+        mem = new int[m+1][n+1];
         
-        dp[0][0] = grid[0][0];
+        return find(grid, m, n, mem);
+    }
+    
+    public int find(int grid[][], int m, int n, int[][] mem){
+        if(m<0 || n<0)
+            return Integer.MAX_VALUE;
         
-        //filling 1st row by adding prev values of that row
-        for(int i=1;i<n;i++){
-            dp[0][i]= dp[0][i-1]+grid[0][i];
-        }
+        else if(m==0 && n== 0)
+            return grid[0][0];
         
+        else if(mem[m][n] != 0)
+            return mem[m][n];
         
-        //filing 1st col..
-        for(int i=1;i<m;i++){
-            dp[i][0] = dp[i-1][0]+ grid[i][0];
-        }
-        
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
-            }
-        }
-        
-        return dp[m-1][n-1];
+        return mem[m][n]= grid[m][n] + Math.min(find(grid, m-1, n, mem), find(grid,m,n-1, mem));
     }
 }
