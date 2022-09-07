244e0e1b18fecb4f352949990cb0cbb5b0963ca5
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-09-07 12:49:58 +0530

    Time: 1 ms (89.85%), Space: 43.8 MB (33.47%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -8,6 +8,8 @@ class Solution {
         dp[1] = cost[1];
         
         for(int i=2;i<=n;i++){
+            //start from index 2 and add min btw prev 2 values 
+            //if i=n ie reached to top beyond last step so add 0 and return dp[n]
             dp[i]= Math.min(dp[i-1], dp[i-2]) + (i==n ? 0 : cost[i]);
         }
         
