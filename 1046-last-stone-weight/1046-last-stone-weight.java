20b1e4eb6f9402afad757362a326e7aa8c1d04f3
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-09-11 12:25:31 +0530

    Time: 2 ms (82.63%), Space: 41.4 MB (68.80%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -4,15 +4,7 @@ class Solution {
         Arrays.sort(stones);
         
         for(int i=stones.length-1;i>0;i--){
-            if(stones[i]==stones[i-1]){
-                if(i-1>0)
-                    i -=1;
-                else
-                    return 0;
-            }
-            else{
-                stones[i-1] = stones[i]-stones[i-1];
-            }
+            stones[i-1] = stones[i]-stones[i-1];
             Arrays.sort(stones);
             
         }
