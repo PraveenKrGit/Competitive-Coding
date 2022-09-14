6671f531d0f77feda4c2f6429e878430a1592ac9
Praveen Kumar <30688435+PraveenKrGit@users.noreply.github.com>
2022-09-14 18:16:18 +0530

    Time: 15 ms (45.74%), Space: 55.5 MB (69.31%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -10,12 +10,14 @@ class Solution {
             char[] ch = s.toCharArray();
             
             Arrays.sort(ch);
+            //String.valueOf(char array) returns the array in String format
+            //i.e. ['p','r','a'] means it returns pra,
             String Key = String.valueOf(ch);
             
             if(!map.containsKey(Key))
-                map.put(Key, new ArrayList<>());
+                map.put(Key, new ArrayList<>()); //adds empty arraylist to key pra
             
-            map.get(Key).add(s);
+            map.get(Key).add(s);  //add that string s to arraylist of key pra
         }
         
         return new ArrayList<>(map.values());
