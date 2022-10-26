b8b5f745548841ef666c4f02e769380421013db6
Praveen Kumar Yadav <30688435+PraveenKrGit@users.noreply.github.com>
2022-10-26 15:43:25 +0530

    Time: 1 ms (8.48%), Space: 42.2 MB (8.25%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,14 +1,43 @@
-//recursion
+//iterative
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null)
-            return true;
-        if(p==null || q==null)
-            return false;
+        Deque<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(p);
+        queue.add(q);
         
-        if(p.val != q.val)
-            return false;
+        while(!queue.isEmpty()){
+            TreeNode pp = queue.poll();
+            TreeNode qq = queue.poll();
+            
+            if(pp == null && qq==null)
+                continue;
+            if(pp==null || qq==null)
+                return false;
+            if(pp.val != qq.val)
+                return false;
+            
+            queue.add(pp.left);
+            queue.add(qq.left);
+            queue.add(pp.right);
+            queue.add(qq.right);
+        }
         
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return true;
     }
 }
+
+
+//recursion
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if(p==null && q==null)
+//             return true;
+//         if(p==null || q==null)
+//             return false;
+        
+//         if(p.val != q.val)
+//             return false;
+        
+//         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }
